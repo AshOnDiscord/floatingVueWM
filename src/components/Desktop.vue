a0b5a1cfedd58cfe00687db7fcec1b3f591c82c9
AshOnDiscord <68016221+AshOnDiscord@users.noreply.github.com>
2024-04-19 00:15:56 -0400

    Component Array Rewrite

---
+++ src/components/Desktop.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { TrashIcon } from '@heroicons/vue/16/solid';
+</script>
+
+<template>
+  <ul id="desktop"
+    class="grid grid-cols-[repeat(auto-fill,5rem)] grid-rows-[repeat(auto-fill,6rem)] w-screen h-full grid-flow-col gap-2 py-2">
+    <li v-for="_ in 10">
+      <button class="flex flex-col items-center justify-start hover:!bg-white/10 p-1 focus:bg-white/20">
+        <TrashIcon class="h-8 w-8 text-white drop-shadow-[0_1px_1px_rgba(0,0,0,0.25)]" />
+        <p class="text-white drop-shadow-[0_1px_1px_rgba(0,0,0,1)] text-center">Recycle Bin</p>
+      </button>
+    </li>
+  </ul>
+</template>
---
+++ src/components/WindowComponent.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const { props, children } = defineProps<{
+  props: {
+    id: string
+    title: string
+    icon: string
+  }
+  children: any
+}>();
+
+</script>
+
+<template>
+  <div class="absolute bg-white grid grid-rows-[min-content,auto] w-[50vw] h-[50vh] rounded-lg overflow-hidden">
+    <nav class="flex justify-between bg-slate-200 px-2 py-1">
+      <div class="flex gap-2">
+        <img :src="props.icon" class="h-6 w-6" />
+        <span>{{ props.title }}</span>
+      </div>
+      <ul class="grid grid-cols-3">
+        <li class="flex justify-center items-center"><button>-</button></li>
+        <li class="flex justify-center items-center"><button>O</button></li>
+        <li class="flex justify-center items-center"><button>X</button></li>
+      </ul>
+    </nav>
+    <main class="overflow-hidden pl-2 pr-1 py-1 ">
+      <div class="overflow-scroll h-full pr-1">
+        <component :is="children" />
+      </div>
+    </main>
+  </div>
+</template>;
